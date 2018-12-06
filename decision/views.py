0949ef1f289628b8ec35fcca270a0ff562fe9ba5
C-Shi <cshi@ualberta.ca>
2018-12-06 17:00:00 +0800

    setup project

---
+++ decision/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# RESTFUL index page
+def index(request):
+  return render(request, 'decision/index.html')
+
+# RESTFUL get NEW form page
+def new(request):
+  pass
+
+# RESTFUL post Create page
+def create(request):
+  pass
+
+# RESTFUL show route for individual decision
+def show(request, decision_id):
+  pass
+
+# RESTFUL post route to delete
+def delete(request, decision):
+  pass
