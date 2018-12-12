f82f58ae21643d8898da34b1dfb456480c7edf8c
C-Shi <cshi@ualberta.ca>
2018-12-12 23:11:59 +0800

    restful create test

--- decision/tests.py
+++ decision/tests.py
@@ -1,6 +1,7 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Activity, Choice
 
@@ -47,4 +48,26 @@ class ChoiceModelTests(TestCase):
     choice_list = create_fake_choice(activity)
     for choice in choice_list:
       self.assertEqual(choice.votes_count, 0)
+
+class IndexViewTests(TestCase):
+  def test_get_index_page(self):
+    response = self.client.get(reverse('decision:index'))
+    self.assertEqual(response.status_code, 200)
+
+class NewRouteTest(TestCase):
+  def test_new_page(self):
+    response = self.client.get(reverse('decision:new'))
+    self.assertEqual(response.status_code, 200)
+  
+  def test_new_page_elements(self):
+    response = self.client.get(reverse('decision:new'))
+    self.assertContains(response, 'Activity')
+    self.assertContains(response, 'Your Name')
+    self.assertContains(response, 'Email')
+    self.assertContains(response, 'Description')
+  
+  def test_create_new_activity_redirect(self):
+    response = self.client.post(reverse('decision:create'), data={'context': 'Hello World', 'creator': 'admin', 'email':'admin@home.com', 'description': 'description'})
+    self.assertEqual(response.status_code, 302)
+
 
